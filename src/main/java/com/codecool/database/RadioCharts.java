8c1f183450bfb23b82d245eaa7f6714457965f37
Horváth Viktor <karlsasse@gmail.com>
2021-03-24 17:59:56 +0100

    megoldás 2021.03.24.17:56

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,13 +1,9 @@
 package com.codecool.database;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
-
 public class RadioCharts {
     RadioCharts(String url, String user, String password) {
-        connectToDataBase(url, user, password);
+        connectDataBase(url, user, password);
     }
 
     public String getMostPlayedSong() {
@@ -18,25 +14,7 @@ public class RadioCharts {
         return "";
     }
 
-    private void connectToDataBase(String url, String user, String password) {
-        Connection conn = null;
-        Properties connectionProps = new Properties();
-        connectionProps.put("user", user);
-        connectionProps.put("password", password);
-
-        if (this.dbms.equals("mysql")) {
-            conn = DriverManager.getConnection(
-                    "jdbc:" + this.dbms + "://" +
-                            this.serverName +
-                            ":" + this.portNumber + "/",
-                    connectionProps);
-        } else if (this.dbms.equals("derby")) {
-            conn = DriverManager.getConnection(
-                    "jdbc:" + this.dbms + ":" +
-                            this.dbName +
-                            ";create=true",
-                    connectionProps);
-        }
-        System.out.println("Connected to database");
+    private void connectDataBase(String url, String user, String password) {
+        
     }
 }
